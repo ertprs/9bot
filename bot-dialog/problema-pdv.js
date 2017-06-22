5f1f90bdd35fd97b3ac97eec7e6f5032e298ccf0
saulooliveira <saulo.oliveira@lasa.com.br>
2017-06-22 20:23:30 -0300

    Dicionário de regras 1.2 incluindo PDV

--- bot-dialog/problema-pdv.js
+++ bot-dialog/problema-pdv.js
@@ -2,13 +2,22 @@ const util = require('./util');
 module.exports = [
     {
         "id": "problema_pdv",
-        "priority":-9999,
         "scoreRule": {
+            "intent": "problema_pdv",
+            "entities": {
+                "problema_loja": "pdv"
+            }
         },
         "action": {
             "reply": [
-                "Não entendi o que você quis dizer, tente reformular sua frase por favor.\n\n"+
-                "Em caso de dúvidas do que pode fazer, é só pedir por ajuda."
+                "Antes de abrirmos um chamado, siga os procedimentos abaixo:",
+                "1-Verifique se o cabo está devidamente conectado",
+                util.replyStaticMedia("pdv1.png"),
+                "2-Verifique se os componentes estão encaixados",
+                util.replyStaticMedia("pdv2.jpg"),
+                "3-Verifique se o cabo de força está conectado",
+                util.replyStaticMedia("pdv3.jpg")
+
             ],
             "defineContext": { "intent": null, "entities": null },
             "listenTo": [
