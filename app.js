f69cc89f5eb4a3709a76a79ebf7acba4536c7dbc
tnobrega <tiago.nobrega@lasa.com.br>
2017-06-30 16:02:18 -0300

    adicionado plunk para implementação

--- app.js
+++ app.js
@@ -55,6 +55,64 @@ let runVersion = function (session) {
     return false;
 };
 
+let runMessageTypes = function (session) {
+    if (session.message.text === '_card') {
+        displayCarousel(session);
+        return true;
+    }
+    return false;
+};
+
+let displayCarousel = function (session) {
+    let carousselCards = new builder.Message(session)
+        .attachmentLayout(builder.AttachmentLayout.carousel)
+        .attachments(
+            [
+                new builder.HeroCard(session)
+                    .title('Azure Storage')
+                    .subtitle('Offload the heavy lifting of data center management')
+                    .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
+                    .images([
+                        builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/storage/media/storage-introduction/storage-concepts.png')
+                    ])
+                    .buttons([
+                        builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+                    ]),
+                new builder.ThumbnailCard(session)
+                    .title('DocumentDB')
+                    .subtitle('Blazing fast, planet-scale NoSQL')
+                    .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
+                    .images([
+                        builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/documentdb/media/documentdb-introduction/json-database-resources1.png')
+                    ])
+                    .buttons([
+                        builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
+                    ]),
+                new builder.HeroCard(session)
+                    .title('Azure Functions')
+                    .subtitle('Process events with a serverless code architecture')
+                    .text('An event-based serverless compute experience to accelerate your development. It can scale based on demand and you pay only for the resources you consume.')
+                    .images([
+                        builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-5daae9212bb433ad0510fbfbff44121ac7c759adc284d7a43d60dbbf2358a07a/images/page/services/functions/01-develop.png')
+                    ])
+                    .buttons([
+                        builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
+                    ]),
+                new builder.ThumbnailCard(session)
+                    .title('Cognitive Services')
+                    .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
+                    .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
+                    .images([
+                        builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-68b530dac63f0ccae8466a2610289af04bdc67ee0bfbc2d5e526b8efd10af05a/images/page/services/cognitive-services/cognitive-services.png')
+                    ])
+                    .buttons([
+                        builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/cognitive-services/', 'Learn More')
+                    ])
+            ]
+        );
+    session.send(carousselCards);
+};
+
 
 let sendProactiveMessage = function (addr,textMessage) {
 
@@ -73,6 +131,7 @@ let sendProactiveMessage = function (addr,textMessage) {
     //         "name": 'warning.png'
     //     }
     // ]);
+    // bot.send(addr,msg);
 
     let buildMsg = function(text){
         let msg2 = new builder.Message().address(addr);
@@ -124,7 +183,7 @@ bot.dialog('lais', [
         let message = session.message;
         let s = session;
 
-        if ( runReset(session) || runVersion(session) || runNotify(session)){
+        if ( runReset(session) || runVersion(session) || runNotify(session) || runMessageTypes(session) ){
             return;
         }
 
