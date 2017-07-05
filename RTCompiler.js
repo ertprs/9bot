c9426801000c3249f1516275f2ecec5a6667c647
tnobrega <tiago.nobrega@lasa.com.br>
2017-07-05 18:22:34 -0300

    implementação compileAttributes do modulo RTCompiler

--- RTCompiler.js
+++ RTCompiler.js
@@ -9,6 +9,15 @@ const RTCompiler = function(args){
         vm.runInNewContext(code,sandbox);
         return sandbox.module.exports;
     };
+    me.compileAttributes = (obj,...attrs)=>{
+        attrs.forEach((attr)=>{
+            let attrVal = obj[attr];
+            if(!attrVal) return;
+            if(!(typeof attrVal === "string")) throw new Error(`Cannot compile attribute ${attr}. Only strings allowed`);
+            let code = `module.exports = ${attrVal}`;
+            obj[attr] = me.require(code);
+        })
+    };
     return me;
 };
 module.exports = RTCompiler;
--- compilerTest.js
+++ compilerTest.js
@@ -6,43 +6,19 @@ const RTCompiler = require('./RTCompiler')({get,add,clear,clearAll, util, _, mom
 
 const laisConv = require('./_extra/lais-conversation-definition/joinedRules_sample');
 
-let compileAttrs = (obj,...attrs)=>{
-    attrs.forEach((attr)=>{
-        let attrVal = obj[attr];
-        if(!attrVal) return;
-        if(!(typeof attrVal === "string")) throw new Error(`Cannot compile attribute ${attr}. Only strings allowed`);
-        let code = `module.exports = ${attrVal}`;
-        obj[attr] = RTCompiler.require(code);
-    })
-};
-
 let compileFunctions = (conv)=>{
     conv.rules = conv.rules.map((r)=>{
         console.log("compiling rule: "+r.id+" ...");
-        compileAttrs(r,"match");
+        RTCompiler.compileAttributes(r,"match");
 
         r.actions.forEach((a,i)=>{
             console.log("compiling action: "+r.id+".actions["+i+"] ...");
-            compileAttrs(a,"match"/*,"goToDialog"*/);//não tem como saber se o goToDialog é uma string com o id do dialogo ou uma string com uma função;
+            RTCompiler.compileAttributes(a,"match"/*,"goToDialog"*/);//não tem como saber se o goToDialog é uma string com o id do dialogo ou uma string com uma função;
         });
         return r;
     });
 };
 
-
-
 console.log('------------------- Compiling rules ------------------');
 compileFunctions(laisConv);
-
-
-// laisConv.rules.forEach((r)=>{
-//     let matchString = r.match || "()=>true";
-//     let code = `module.exports = ${matchString}`;
-//     console.log("compiling "+r.id+"...");
-//     let func = RTCompiler.require(code);
-//     console.log("running...");
-//     func({});
-//     console.log("done!");
-// });
-
 console.log('########################################################');
