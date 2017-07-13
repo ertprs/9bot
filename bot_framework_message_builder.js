9c5ed59b04c759d334aa54674d5d8485002503f5
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2017-07-13 16:41:05 -0300

    Adicionando a resposta com mídia no BotFrameworkMessageBuilder.

--- bot_framework_message_builder.js
+++ bot_framework_message_builder.js
@@ -8,6 +8,8 @@ class BotFrameworkMessageBuilder {
   build(session, reply, context) {
     if(this.isTextReply(reply)) {
       return this.buildTextReply(session, reply, context);
+    } else if(this.isMediaReply(reply)) {
+      return this.buildMediaReply(session, reply, context);
     } else {
       throw new Error("Tipo de resposta não suportado: " + typeof(reply) +
         ". A resposta deve ser uma String (texto) ou um objeto (escolha ou mídia).");
@@ -19,8 +21,25 @@ class BotFrameworkMessageBuilder {
     return message.text(laisDictionary.resolveWithContext(reply, context))
   }
 
+  buildMediaReply(session, reply, context) {
+    let meta = reply.meta || {};
+    let layout = meta.layout || builder.AttachmentLayout.carousel;
+
+    if(!_.isArray(reply.content)) {
+      reply.content = [reply.content];
+    }
+
+    return new builder.Message(session)
+      .attachmentLayout(layout)
+      .attachments(reply.content);
+  }
+
   isTextReply(reply) {
     return _.isString(reply);
+  }
+
+  isMediaReply(reply) {
+    return _.isObject(reply) && reply.type == "media";
   }
 }
 
