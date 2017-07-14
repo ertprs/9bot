224993d0de25f6d357cc11387e244eed93c0a666
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2017-07-14 16:21:48 -0300

    Adicionando no BotFrameworkMessageBuilder a possibilidade de responder com escolhas.

--- bot_framework_message_builder.js
+++ bot_framework_message_builder.js
@@ -10,6 +10,8 @@ class BotFrameworkMessageBuilder {
       return this.buildTextReply(session, reply, context);
     } else if(this.isMediaReply(reply)) {
       return this.buildMediaReply(session, reply, context);
+    } else if(this.isChoiceReply(reply)) {
+      return this.buildChoiceReply(session, reply, context);
     } else {
       throw new Error("Tipo de resposta não suportado: " + typeof(reply) +
         ". A resposta deve ser uma String (texto) ou um objeto (escolha ou mídia).");
@@ -34,12 +36,66 @@ class BotFrameworkMessageBuilder {
       .attachments(reply.content);
   }
 
+  buildChoiceReply(session, reply, context) {
+    if(!_.isArray(reply.content)) {
+      throw new Error('Conteúdo inválido para resposta do tipo escolha. Deve-se fornecer um array.');
+    }
+
+    let meta = reply.meta || {};
+    let layout = meta.layout || builder.AttachmentLayout.list;
+
+    let message = new builder.Message(session).attachmentLayout(layout);
+
+    let card = new builder.HeroCard(session);
+
+    if(meta.title) {
+      card = card.title(meta.title.toString());
+    }
+    if(meta.subtitle) {
+      card = card.subtitle(meta.subtitle.toString());
+    }
+    if(meta.text) {
+      card = card.text(meta.text.toString());
+    }
+
+    let cardActions = reply.content.map((replyContent) => {
+      let text, value;
+
+      if(_.isString(replyContent)) {
+        text = value = laisDictionary.resolveWithContext(replyContent, context);
+      } else if(_.isObject(replyContent)) {
+        if(replyContent.text) {
+          text = laisDictionary.resolveWithContext(replyContent.text, context)
+        } else {
+          text = "Não definido";
+        }
+
+        if(replyContent.value) {
+          value = laisDictionary.resolveWithContext(replyContent.value, context)
+        } else {
+          value = text;
+        }
+      } else {
+        throw new Error('Opção inválida para resposta do tipo escolha. ' +
+          'Deve-se fornecer um objeto (com as propriedades text e value) ou uma string.');
+      }
+
+      return builder.CardAction.imBack(session, value, text);
+    });
+
+    return message.attachments([card.buttons(cardActions)]);
+  }
+
   isTextReply(reply) {
     return _.isString(reply);
   }
 
   isMediaReply(reply) {
     return _.isObject(reply) && reply.type == "media";
+  }
+
+  isChoiceReply(reply) {
+    return _.isObject(reply) && reply.type == "choice";
   }
 }
 
